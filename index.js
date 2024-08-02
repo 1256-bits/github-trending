cc9848f1f138657db56806b16737a9e6c6580ed6
128bit <128bit@eclipso.eu>
2024-08-02 19:11:26 +0300

    Fix pruneDatabase being unused

--- index.js
+++ index.js
@@ -143,6 +143,7 @@ async function mainLoop () {
     const data = await getTrendingRepos()
     const dataProcessed = data.map(item => `('${item.id}', '${item.name}', '${item.owner}', '${item.language}', '${item.stars}')`).join(',')
     db.run(`INSERT INTO repos VALUES ${dataProcessed} ON CONFLICT DO UPDATE SET stars = excluded.stars`)
+    db.get("SELECT COUNT(*) AS 'length' FROM repos", pruneDatabase)
   } catch {
     console.error("Fetch failed. Entering offline mode")
     offline = true
